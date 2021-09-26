b9f398b6d3102f1e5b39b1e16696680f92ccf173
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2021-09-26 18:49:11 +0200

    Add empty projects

---
+++ client/src/main/java/com/github/ai/fprovider/demo/App.kt
@@ -0,0 +1,6 @@
+package com.github.ai.fprovider.demo
+
+import androidx.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+}
---
+++ client/src/main/java/com/github/ai/fprovider/demo/ClientActivity.kt
@@ -0,0 +1,13 @@
+package com.github.ai.fprovider.demo
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class ClientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(View(this))
+    }
+}
