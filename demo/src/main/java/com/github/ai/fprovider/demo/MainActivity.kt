0519a4b993e2b84c6e3d34fb6bb23d3b387cd341
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2021-10-22 20:36:30 +0200

    [Demo] Add token support

--- demo/src/main/java/com/github/ai/fprovider/demo/MainActivity.kt
+++ demo/src/main/java/com/github/ai/fprovider/demo/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
@@ -14,14 +15,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.github.ai.fprovider.InternalStorageTokenManager
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels(
         factoryProducer = {
             MainViewModelFactory(
-                FileSystemInteractor(applicationContext)
+                FileSystemInteractor(applicationContext),
+                InternalStorageTokenManager(applicationContext)
             )
         }
     )
@@ -39,19 +45,31 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun ActivityScreen(viewModel: MainViewModel) {
     val state: Boolean by viewModel.isProgressVisible.observeAsState(false)
+    val accessToken: String by viewModel.accessToken.observeAsState("")
     ScreenLayout(
         isProgressVisible = state,
-        onButtonClicked = { viewModel.createFileStructure() }
+        accessToken = accessToken,
+        onGenerateStructureClicked = { viewModel.createFileStructure() },
+        onGenerateTokenClicked = { viewModel.generateAccessToken() }
     )
 }
 
 @Composable
 fun ScreenLayout(
     isProgressVisible: Boolean,
-    onButtonClicked: (() -> Unit)? = null
+    accessToken: String,
+    onGenerateStructureClicked: (() -> Unit)? = null,
+    onGenerateTokenClicked: (() -> Unit)? = null
 ) {
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (button, progress) = createRefs()
+        val (fileStructureButton, tokenButton, tokenText, progress) = createRefs()
+
+        createVerticalChain(
+            fileStructureButton,
+            tokenButton,
+            tokenText,
+            chainStyle = ChainStyle.Packed
+        )
 
         if (isProgressVisible) {
             CircularProgressIndicator(
@@ -64,15 +82,44 @@ fun ScreenLayout(
             )
         } else {
             Button(
-                onClick = { onButtonClicked?.invoke() },
-                modifier = Modifier.constrainAs(button) {
+                onClick = { onGenerateStructureClicked?.invoke() },
+                modifier = Modifier.constrainAs(fileStructureButton) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
+                    bottom.linkTo(tokenButton.top)
                 }
             ) {
                 Text(text = "Create file structure")
+            }
+
+            Button(
+                onClick = { onGenerateTokenClicked?.invoke() },
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .constrainAs(tokenButton) {
+                        top.linkTo(fileStructureButton.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(tokenText.top)
+                    }
+            ) {
+                Text(text = "Set new token")
+            }
+
+            if (accessToken.isNotEmpty()) {
+                Text(
+                    text = "New access token is set: $accessToken",
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(top = 12.dp)
+                        .constrainAs(tokenText) {
+                            top.linkTo(tokenButton.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        }
+                )
             }
         }
     }
@@ -82,6 +129,9 @@ fun ScreenLayout(
 @Composable
 fun LayoutPreview() {
     MaterialTheme {
-        ScreenLayout(isProgressVisible = false)
+        ScreenLayout(
+            isProgressVisible = false,
+            accessToken = "abc-123"
+        )
     }
 }
--- demo/src/main/java/com/github/ai/fprovider/demo/MainViewModel.kt
+++ demo/src/main/java/com/github/ai/fprovider/demo/MainViewModel.kt
@@ -5,12 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.github.ai.fprovider.InternalStorageTokenManager
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
-class MainViewModel(private val interactor: FileSystemInteractor) : ViewModel() {
+class MainViewModel(
+    private val interactor: FileSystemInteractor,
+    private val tokenManager: InternalStorageTokenManager
+) : ViewModel() {
 
     private val _isProgressVisible = MutableLiveData(false)
     val isProgressVisible: LiveData<Boolean> = _isProgressVisible
+
+    private val _accessToken = MutableLiveData("")
+    val accessToken: LiveData<String> = _accessToken
 
     fun createFileStructure() {
         _isProgressVisible.value = true
@@ -20,13 +28,39 @@ class MainViewModel(private val interactor: FileSystemInteractor) : ViewModel() 
             _isProgressVisible.value = false
         }
     }
+
+    fun generateAccessToken() {
+        _isProgressVisible.value = true
+
+        viewModelScope.launch {
+            tokenManager.removeAllTokens()
+
+            val newToken = newToken()
+            tokenManager.addToken(newToken)
+
+            _accessToken.value = newToken
+            _isProgressVisible.value = false
+        }
+    }
+
+    private fun newToken(): String {
+        val value = Random(System.currentTimeMillis()).nextInt(10)
+
+        val token = StringBuilder()
+        for (idx in 0 until 4) {
+            token.append(value)
+        }
+
+        return token.toString()
+    }
 }
 
 class MainViewModelFactory(
-    private val interactor: FileSystemInteractor
+    private val interactor: FileSystemInteractor,
+    private val tokenManager: InternalStorageTokenManager
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return MainViewModel(interactor) as T
+        return MainViewModel(interactor, tokenManager) as T
     }
 }
